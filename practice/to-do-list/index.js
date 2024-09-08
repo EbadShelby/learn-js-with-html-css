585baf1736d04e2e5302a8f819d89876f401c71c
EbadShelby <ebadyasser123xd@gmail.com>
2024-09-08 13:14:58 +0800

    learned arrays and loops with simple todolist project

--- practice/to-do-list/index.js
+++ practice/to-do-list/index.js
@@ -16,7 +16,7 @@ function renderTodoList() {
   for (let i = 0; i < todoList.length; i++) {
     const todoObject = todoList[i];
     const { name, dueDate } = todoObject;
-    const html = `<p>${name} ${dueDate}<button onclick='todoList.splice(${i}, 1);renderTodoList()'>Delete</button></p>`;
+    const html = `<div>${name}</div><div>${dueDate}</div><button class="delete-todo-button"  onclick='todoList.splice(${i}, 1);renderTodoList()'>Delete</button>`;
     todoListHTML += html;
   }
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
@@ -25,9 +25,15 @@ function renderTodoList() {
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
   const name = inputElement.value;
+  const dateElement = document.querySelector(".js-date");
+  const dueDate = dateElement.value;
 
-  todoList.push(name);
+  todoList.push({
+    name,
+    dueDate,
+  });
   inputElement.value = "";
+  dateElement.value = "";
 
   renderTodoList();
 }
