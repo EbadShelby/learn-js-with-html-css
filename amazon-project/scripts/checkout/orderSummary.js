a9058edb3797ed167bf414a25ba968e4332eaa05
EbadShelby <ebadyasser123xd@gmail.com>
2024-09-29 10:26:46 +0800

    done 15k

--- amazon-project/scripts/checkout/orderSummary.js
+++ amazon-project/scripts/checkout/orderSummary.js
@@ -13,6 +13,7 @@ import {
   getDeliveryOption,
 } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
+import { renderCheckoutHeader } from "./checkoutHeader.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
@@ -128,6 +129,7 @@ export function renderOrderSummary() {
       const productId = link.dataset.productId;
       removeFromCart(productId);
 
+      renderCheckoutHeader();
       renderOrderSummary();
       renderPaymentSummary();
     });
